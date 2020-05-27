71efb197f1bf2e5abf905b528ced746a65723ae6
Prateek <codes.prateek@gmail.com>
2020-05-27 10:28:46 +0530

    Quiz fragment in action

--- app/src/main/java/com/androidmonk/quizapp/DetailsFragment.java
+++ app/src/main/java/com/androidmonk/quizapp/DetailsFragment.java
@@ -5,17 +5,39 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailsFragment extends Fragment {
+public class DetailsFragment extends Fragment implements View.OnClickListener {
+
+    private NavController navController;
+    private QuizListViewModel quizListViewModel;
+    private int position;
+    private ImageView detailsImage;
+    private TextView detailsTitle;
+    private TextView detailsDesc;
+    private TextView detailsDiff;
+    private TextView detailsQuestions;
+
+    private Button detailsStartButton;
 
     public DetailsFragment() {
         // Required empty public constructor
@@ -33,7 +55,52 @@ public class DetailsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        int position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
-        Log.d("APP_LOG", "Position: " + position);
+        position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
+        //Log.d("APP_LOG", "Position: " + position);
+
+        navController = Navigation.findNavController(view);
+
+        //Initialize the UI Element
+        detailsImage = view.findViewById(R.id.details_image);
+        detailsTitle = view.findViewById(R.id.details_title);
+        detailsDesc = view.findViewById(R.id.details_desc);
+        detailsDiff = view.findViewById(R.id.details_difficulty_text);
+        detailsQuestions = view.findViewById(R.id.details_questions_text);
+        detailsStartButton = view.findViewById(R.id.details_start_btn);
+
+        detailsStartButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        quizListViewModel = new ViewModelProvider(getActivity()).get(QuizListViewModel.class);
+        quizListViewModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+
+                detailsTitle.setText(quizListModels.get(position).getName());
+                detailsDesc.setText(quizListModels.get(position).getDesc());
+                detailsDiff.setText(quizListModels.get(position).getLevel());
+                detailsQuestions.setText(quizListModels.get(position).getQuestions()+ "");
+
+                Glide.with(getContext())
+                        .load(quizListModels.get(position).getImage())
+                        .centerCrop()
+                        .placeholder(R.drawable.placeholder_image)
+                        .into(detailsImage);
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.details_start_btn:
+                navController.navigate(R.id.action_detailsFragment_to_quizFragment);
+                break;
+        }
     }
 }
