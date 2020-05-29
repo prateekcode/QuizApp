2194caee87a1742788855189d63c726b12191abd
Prateek <codes.prateek@gmail.com>
2020-05-29 21:09:28 +0530

    ResultFragment class initialize

--- app/src/main/java/com/androidmonk/quizapp/ResultFragment.java
+++ app/src/main/java/com/androidmonk/quizapp/ResultFragment.java
@@ -2,17 +2,44 @@ package com.androidmonk.quizapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ResultFragment extends Fragment {
+
+    private NavController navController;
+
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+    private String currentUserId;
+
+    private String quizId;
+
+    private TextView resultCorrect;
+    private TextView resultWrong;
+    private TextView resultMissed;
+
+    private TextView resultPercent;
+    private ProgressBar resultProgress;
+
+    private Button resultHomeBtn;
+
 
     public ResultFragment() {
         // Required empty public constructor
@@ -24,5 +51,11 @@ public class ResultFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
