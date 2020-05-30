f5474ecd59cdc61718ce4000cc0875c046f272a9
Prateek <codes.prateek@gmail.com>
2020-05-30 22:09:40 +0530

    Bug Fixed

--- app/src/main/java/com/androidmonk/quizapp/Activity/MainActivity.java
+++ app/src/main/java/com/androidmonk/quizapp/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.androidmonk.quizapp;
+package com.androidmonk.quizapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.androidmonk.quizapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
